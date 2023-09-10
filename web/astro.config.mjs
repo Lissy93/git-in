14854e16220c2b913e06cf3369a35ab70b14f4c8
Alicia Sykes <alicia@omg.lol>
2023-09-10 15:35:35 +0100

    Creates an Astro site for tutorials

---
+++ web/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: true,
+  integrations: [mdx(), tailwind(), compress()],
+})
---
+++ web/src/data/guides.ts
@@ -0,0 +1,35 @@
+
+const guides = [
+  {
+    title: 'Submit your first PR (UI)',
+    description: 'Learn how to submit your first pull-request via the GitHub UI.',
+    slug: 'submit-your-first-pr-ui/',
+    icon: 'fa:github',
+  },
+  {
+    title: 'Submit your first PR (code)',
+    description: 'Learn how to submit your first code change to a project.',
+    slug: 'submit-your-first-pr-code/',
+    icon: 'fa:code',
+  },
+  {
+    title: 'Other open source contributions',
+    description: 'See all the other ways you can contribute to open source, without coding.',
+    slug: 'other-open-source-contributions/',
+    icon: 'fa:heart',
+  },
+  {
+    title: 'Why Open Source?',
+    description: 'How open source can help you learn and grow as a developer.',
+    slug: 'why-open-source/',
+    icon: 'fa:question',
+  },
+  {
+    title: 'Open Source for Businesses',
+    description: 'See all the ways embracing open source can benefit your business.',
+    slug: 'open-source-for-businesses/',
+    icon: 'fa:money',
+  },
+];
+
+export default guides;
