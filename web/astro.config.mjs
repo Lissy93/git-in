258f3fcbe2b49ef9db9d8a47dcf24dc96147189d
Alicia Sykes <alicia@omg.lol>
2023-09-24 16:28:15 +0100

    Updates Astro config to serve up from sub-page

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,6 +5,8 @@ import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lissy93.github.io',
+  base: '/git-into-open-source',
   compressHTML: true,
   integrations: [mdx(), tailwind(), compress()],
 })
