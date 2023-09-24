7102eb48b2601b3aed3c06dd71cb166312d3dddb
Alicia Sykes <alicia@omg.lol>
2023-09-24 16:43:21 +0100

    Moved definitions of base url to env vars

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,10 +3,14 @@ import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 
+
+const site = process.env.SITE_URL || undefined;
+const base = process.env.BASE_PATH || '/';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lissy93.github.io',
-  base: '/git-into-open-source',
+  site,
+  base,
   compressHTML: true,
   integrations: [mdx(), tailwind(), compress()],
 })
