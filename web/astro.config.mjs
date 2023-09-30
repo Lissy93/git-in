13b1d3122181c4e700ea379d97a5f3a03d2b183c
Alicia Sykes <alicia@omg.lol>
2023-09-30 13:06:56 +0100

    Adds sitemap generation

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
+import dotenv from 'dotenv';
+import sitemap from '@astrojs/sitemap';
 
+dotenv.config();
 
 const site = process.env.SITE_URL || undefined;
 const base = process.env.BASE_PATH || '/';
 
-// https://astro.build/config
 export default defineConfig({
   site,
   base,
   compressHTML: true,
-  integrations: [mdx(), tailwind(), compress()],
+  integrations: [mdx(), tailwind(), compress(), sitemap()],
 })
